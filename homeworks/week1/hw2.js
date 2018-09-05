05cca65d56992ccd8c7327f0810cd98bea00c1fd
Terri <trchen106@gmail.com>
2018-09-06 06:29:13 +0800

    changed from 'console.log' to 'return'

--- homeworks/week1/hw2.js
+++ homeworks/week1/hw2.js
@@ -2,7 +2,7 @@ function capitalize(str) {
     var firstChar = str.charAt(0).toUpperCase()
     var otherChar = str.slice(1)
     var newStr = firstChar+otherChar;
-    console.log(newStr);
+    return(newStr);
 }
   
 //JavaScript String charAt() Method:Return the first character of a string.
--- homeworks/week1/hw5.js
+++ homeworks/week1/hw5.js
@@ -4,12 +4,12 @@ function join(str, concatStr) {
         newStr += str[i]+concatStr ; 
     }
     newStr += str[str.length-1];
-    console.log(newStr);
+    return(newStr);
 }
 
 function repeat(str, times) {
     var newStr = '';
     for(var i=1; i<=times; i++)
         newStr += str;
-    console.log(newStr);
+    return(newStr);
 }
